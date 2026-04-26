d3cd876cf96fa4670682caca2b884e75b3a3b05d
Rafique610 <rafiquecodes@gmaill.com>
2026-04-26 14:17:40 +0500

    feat: implement user model with password hashing

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema({
+name: {
+type: String,
+required: true,
+},
+email: {
+type: String,
+required: true,
+unique: true,
+},
+password: {
+type: String,
+required: true,
+},
+role: {
+type: String,
+enum: ["admin", "agent"],
+default: "agent",
+},
+}, { timestamps: true });
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+if (!this.isModified("password")) return next();
+
+const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password, salt);
+
+next();
+});
+
+// Compare password (for login)
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
